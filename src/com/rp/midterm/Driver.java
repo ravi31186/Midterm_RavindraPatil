36c12a9f31dccc60151dd67d8947f06ff60f9c1f
ravi31186 <ravi31186@gmail.com>
2025-10-15 11:05:32 -0400

    Final Commit

--- src/com/rp/midterm/Driver.java
+++ src/com/rp/midterm/Driver.java
@@ -5,28 +5,29 @@ public class Driver {
 	public static void main(String[] args) {
 				
 		HourlyApplicant ha1 = new HourlyApplicant("Ravi", "Patil", "123456789", 10.75, 40);
-		HourlyApplicant ha2 = new HourlyApplicant("John", "Doe", "5647463843", 15.75, 35);
-		HourlyApplicant ha3 = new HourlyApplicant("Micheal", "Jackson", "984983943", 19, 25);
-		
-		CommissionApplicant ca1 = new CommissionApplicant("David", "Warner", "3646347722", 5000, 2);
-		CommissionApplicant ca2 = new CommissionApplicant("Mike", "Hussey", "4628362833", 19000, 4.5);
-		CommissionApplicant ca3 = new CommissionApplicant("Karan", "Sharma", "475483285", 7000, 9.9);
-
 		System.out.println(ha1);
 		System.out.println("====================");
 		
+		
+		HourlyApplicant ha2 = new HourlyApplicant("John", "Doe", "5647463843", 15.75, 35);
 		System.out.println(ha2);
 		System.out.println("====================");
 		
+		HourlyApplicant ha3 = new HourlyApplicant("Micheal", "Jackson", "984983943", 19, 25);
 		System.out.println(ha3);
 		System.out.println("====================");
 		
+		CommissionApplicant ca1 = new CommissionApplicant("David", "Warner", "3646347722", 5000, 2);
 		System.out.println(ca1);
 		System.out.println("====================");
-
+		
+		
+		CommissionApplicant ca2 = new CommissionApplicant("Mike", "Hussey", "4628362833", 19000, 4.5);
 		System.out.println(ca2);
 		System.out.println("====================");
-
+		
+		
+		CommissionApplicant ca3 = new CommissionApplicant("Karan", "Sharma", "475483285", 7000, 9.9);
 		System.out.println(ca3);
 
 	}
